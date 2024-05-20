c9ba6da8853769c35ab16f7c2799e74a9585998c
Mr_faizan <106084002+faizan-12345@users.noreply.github.com>
2024-05-20 10:01:53 +0500

    Update main.ts

--- 3_name_cases/main.ts
+++ 3_name_cases/main.ts
@@ -1,4 +1,4 @@
-var personName = "Muhammad Faizan";
+var personName = "muhammad Faizan";
 console.log(personName.toLowerCase());
 console.log(personName.toUpperCase());
 console.log(personName.replace(/\b\w/g, function (faisal) { return faisal.toUpperCase(); }));
